fbedb6044ba5413178a95eb69b7a333c6d2baa85
Matthew Morgan <mmorgan@mmorgan-ThinkPad-W530.(none)>
2013-06-10 15:41:41 -0300

    changed routes to Twi config

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//Include the routes
+require('./lib/routes/index')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,39 @@
+//Routes
 
+'use strict';
+
+module.exports = function(app){
+    //Site index
+    app.get('/', function(req, res){
+		res.render('index', { title: 'Express' });
+        });
+
+    //Post routes
+    require('./post')(app);
+
+    //Comment routes
+    require('./comment')(app);
+
+    //Wiki routes
+    require('./wiki')(app);
+
+    //User routes
+    require('./user')(app);
+
+    //Tag routes
+    require('./tag')(app);
+
+    //Upload routes
+    require('./upload')(app);
+
+    //Ajax/Utility Routes
+    require('./misc')(app);
+};
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
+/*exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+*/
