2392ae5a7fdea0d5bce19789119ed0dc26f29823
Liath <liath@exthilion.org>
2013-06-12 02:13:04 +0200

    Oh sweet Celestia, what hath I wrought?

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@
 module.exports = function(app){
     //Site index
     app.get('/', function(req, res){
-		res.render('index', { title: 'Express' });
-        });
+      res.render('index', { title: 'Express' });
+    });
 
     //Post routes
     //require('./post')(app);
