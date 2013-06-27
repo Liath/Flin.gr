4473a285ddf9be4def3c7dd9c7945fb4941a4573
John Jones <liath@exthilion.org>
2013-06-27 16:20:48 -0500

    Basics for the WebRTC bits hacked together

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@
 'use strict';
 
 module.exports = function(app){
+    function idGen() {
+        return (Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1))+(Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1));
+    };
     //Site index
     app.get('/', function(req, res){
       res.render('index', { title: 'Express' });
@@ -9,14 +12,12 @@ module.exports = function(app){
     app.get('/send/:id', function(req, res){
       var id = req.params.id.replace(/\W/g,'');
       res.render('channel', {
-        id : id
+        cid : id,
+        uid: idGen()
       });
     });
     app.get(/\/(send|send\/)/, function(req, res){
-      function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-      };
-      res.redirect('/send/'+s4() + s4());
+      res.redirect('/send/'+idGen());
     });
     //Post routes
     //require('./post')(app);
